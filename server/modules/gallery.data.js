73601aae6ee772e12cedbe5774fb3b0f81e2c50f
Joshua Vedane <jvedane@gmail.com>
2021-01-15 17:36:20 -0600

    updated gallery data and made axios get req

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,11 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/ayinger-brauWeisse.jpg', description: 'Photo of Ayinger Brau Weiße.', likes: 0 },
+    { id: 2, path: 'images/franziskaner-hefeweizenDunkel.jpg', description: 'Photo of Franziskaner Hefeweizen Dunkel.', likes: 0 },
+    { id: 3, path: 'images/hackerPschorr-weisse.jpg', description: 'Photo of Hacker-Pschorr Weisse.', likes: 0 },
+    { id: 4, path: 'images/hofbrau-oktoberfestbier.jpg', description: 'Photo of Hofbrau Oktoberfestbier.', likes: 0 },
+    { id: 5, path: 'images/paulaner-hefeweizen.jpg', description: 'Photo of Paulaner Hefeweizen.', likes: 0 },
+    { id: 6, path: 'images/warsteiner-dunkel.jpg', description: 'Photo of Warsteiner Dunkel.', likes: 0 },
+    { id: 7, path: 'images/weihenstephaner-vitus.jpg', description: 'Photo of Weihenstephaner-vitus.', likes: 0 }
 ];
 
 module.exports = galleryItems;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,45 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import GalleryList from '../GalleryList/GalleryList.jsx'
 
 function App() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
-      </div>
-    );
+  //hooks
+  const [galleryList, setGalleryList] = useState([]);
+
+
+  //routes
+  const getGalleryList = () => {
+    axios({
+      method: 'GET',
+      url: '/gallery'
+    }).then((response) => {
+      console.log('GalleryList from server', response.data);
+      //set galleryList with data from server
+      setGalleryList(response.data);
+    }).catch((error) => {
+      console.log('error on get: ', error);
+    })
+  }
+  //put route
+
+  useEffect(() => {
+    getGalleryList();
+  }, [])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Galerie Deutsches Bier</h1>
+      </header>
+      <GalleryList
+        galleryList={galleryList} // also needs put route 
+      />
+      <p>Gallery goes here</p>
+      <img src="images/ayinger-brauWeisse.jpg" />
+    </div>
+  );
 }
 
 export default App;
