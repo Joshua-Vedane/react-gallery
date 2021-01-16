268e1cf5d18191cea3eba9d66b5fa2dc5f0b6808
Joshua Vedane <jvedane@gmail.com>
2021-01-16 09:19:50 -0600

    base mode logic is complete. time to format styling

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,19 +1,42 @@
-export default function GalleryItem({beer, changeLikes}){
+import { useState } from 'react';
+export default function GalleryItem({ beer, changeLikes }) {
 
-  //click handler
+  //hook for view toggle img/description
+  // false = display image
+  const [toggleView, setToggleView] = useState(false);
+  // toggle view click handler
+  const handleToggleView = () => {
+    setToggleView(!toggleView)
+  }
+
+  const displayToggleView = () => {
+    if (toggleView === false) {
+      return (
+        <img key={beer.id} src={beer.path} alt={beer.description} />
+      )
+    } else {
+      return (
+        <p>{beer.description}</p>
+      )
+    }
+  }
+
+  //Like button click handler
   const handleLikeClick = () => {
     console.log('clicked button', beer.id);
     changeLikes(beer.id);
-    
+
   }
 
   return (
     <>
-    <div className="beer-container">
-      <img key={beer.id} src={beer.path} alt={beer.description}/>
-      <button onClick={handleLikeClick} className="likeBtn btn">Love It!</button>
-      <p>{beer.likes} people love this!</p>
-    </div>
+      <div className="item-container">
+        <div className="item-display" onClick={handleToggleView}>
+          {displayToggleView()}
+        </div>
+        <button onClick={handleLikeClick} className="likeBtn btn">Love It!</button>
+        <p>{beer.likes} people love this!</p>
+      </div>
 
     </>
 
